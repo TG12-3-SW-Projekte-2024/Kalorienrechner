eab71d34d1ba93243f5fc07c96dee2d2e2826ee3
Toffifeyy <123463147+Toffifeyy@users.noreply.github.com>
2024-04-24 19:52:31 +0200

    Update Gui.java

--- Kalorienrechner/Gui.java
+++ Kalorienrechner/Gui.java
@@ -1,23 +1,69 @@
-package Kalorienrechner;
- 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
- 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
- 
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class Gui extends JFrame {
- 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Gui frame = new Gui();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private final JPanel panel = new JPanel();
+	private JCheckBox chckbxNewCheckBox;
+	private JCheckBox chckbxNewCheckBox_1;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gui frame = new Gui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Gui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel = new JLabel("Was ist dein Geschlecht?");
+		contentPane.add(lblNewLabel);
+		contentPane.add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		chckbxNewCheckBox = new JCheckBox("Weiblich");
+		panel.add(chckbxNewCheckBox);
+		
+		chckbxNewCheckBox_1 = new JCheckBox("Männlich");
+		panel.add(chckbxNewCheckBox_1);
+		
+		btnNewButton = new JButton("Weiter");
+		panel.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+	}
+
 }
